7b9e1f338515b5cb32363a316af5cab7d6f649b6
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2023-04-07 14:43:19 +0200

    minor edits

--- dnngior/variables.py
+++ dnngior/variables.py
@@ -13,8 +13,3 @@ MODELS_PATH      = os.path.join(FILES_PATH, "models")
 BIOCHEMISTRY_PATH   = os.path.join(FILES_PATH, "biochemistry")
 MODELSEED_REACTIONS = os.path.join(BIOCHEMISTRY_PATH, "reactions.tsv")
 
-
-
-
-# sys.path.append(BASE)
-
