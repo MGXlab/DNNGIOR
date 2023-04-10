200d4d312c4004fe7334a9718fcd814a2bfc4474
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2023-04-10 23:53:16 +0200

    running example; no reactions added

--- .github/workflows/ci_test.py
+++ .github/workflows/ci_test.py
@@ -11,16 +11,14 @@ params["WLSACCESSID"]= "8e3edcd8-f820-4fe3-bef2-12c072984d6a"
 params["WLSSECRET"]  = "d23976cf-9364-4193-8c5a-c02109353796"
 params["LICENSEID"]  = 962454
 
-with gp.Env(params=params) as env:
-    with gp.Model(env=env) as m:
+gp.Env(params=params) 
 
-
-base_path  = os.getcwd()
-draftModel = os.path.join(base_path, 'S_infantis_mvl3A.sbml')
+base_path  = "/".join(os.getcwd().split("/")[:-2])
+draftModel = os.path.join(base_path, "dnngior/files/models/e_coli_core_Seed.sbml")
 
 gapfill            = dnngior.Gapfill(draftModel, medium = None, objectiveName = 'bio1')
 gf_model_compl_med = gapfill.gapfilledModel.copy()
 
-modelname = os.path.join(base_path, "S_infantis_mvl3A.sbml")
+print("NN gapfilling added {} new readctions".format(len(gapfill.added_reactions)))
+print("The NN gapfilled model, comes with {} reactions and {} metabolites".format(len(gf_model_compl_med.metabolites), len(gf_model_compl_med.reactions)))
 
-cobra.io.write_sbml_model(cobra_model = gf_model_compl_med, filename = modelname)
