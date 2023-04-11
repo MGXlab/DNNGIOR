5ee9c7ec929eb90def5f865ad60ed6fb72daae95
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2023-04-11 13:16:59 +0200

    unzip tarballs when running for 1st time

--- dnngior/__init__.py
+++ dnngior/__init__.py
@@ -1,4 +1,22 @@
 import os
+import tarfile
+from pathlib import Path
+
+path        = Path(os.path.dirname(__file__))
+BiochemRoot = os.path.join(path, 'files', 'biochemistry')
+
+rxns_file = "reactions.tsv"
+ff = Path(os.path.join(BiochemRoot, rxns_file))
+if not ff.is_file():
+    file = tarfile.open(os.path.join(BiochemRoot, 'reactions.tsv.tar.gz'))
+    file.extractall(BiochemRoot)
+
+cpds_file = "compounds.tsv"
+ff = Path(os.path.join(BiochemRoot, cpds_file))
+if not ff.is_file():
+    file = tarfile.open(os.path.join(BiochemRoot, 'compounds.tsv.tar.gz'))
+    file.extractall(BiochemRoot)
+
 import dnngior.files
 
 from dnngior.variables import *
@@ -9,7 +27,6 @@ from dnngior.reaction_class  import Reaction
 
 import dnngior.addExchanges 
 
-from dnngior.gapfill_function import gapfill
 from dnngior.gapfill_class import Gapfill
 
 from dnngior.build_model import refine_model
