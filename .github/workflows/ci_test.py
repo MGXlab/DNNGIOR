bf24d9c513be88dbba8f6258512651a381fee0b6
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2023-04-12 03:04:46 +0200

    test with no prediction step

--- .github/workflows/ci_test.py
+++ .github/workflows/ci_test.py
@@ -1,22 +1,28 @@
 import dnngior
-import cobra
-import os
+from dnngior.reaction_class import Reaction
+from dnngior.NN_Predictor import NN
 
 import gurobipy as gp
 from gurobipy import GRB
+
+import cobra
+import numpy as np
+import os, random
+
 
 params = {}
 params["WLSACCESSID"] = "8e3edcd8-f820-4fe3-bef2-12c072984d6a"
 params["WLSSECRET"]   = "d23976cf-9364-4193-8c5a-c02109353796"
 params["LICENSEID"]   = 962454
 
-gp.Env(params=params) 
+gp.Env(params = params)
 
-base_path  = "/".join(os.path.abspath(__file__).split("/")[:-3])
+base_path = "/".join(os.path.abspath(__file__).split("/")[:-3])
+
+# Draft model
 draftModel = os.path.join(base_path, "docs/models/bh_ungapfilled_model.sbml")
 
-gapfill            = dnngior.Gapfill(draftModel, medium = None, objectiveName = 'bio1')
-gf_model_compl_med = gapfill.gapfilledModel.copy()
-
-print("NN gapfilling added {} new readctions".format(len(gapfill.added_reactions)))
-print("The NN gapfilled model, comes with {} reactions and {} metabolites".format(len(gf_model_compl_med.metabolites), len(gf_model_compl_med.reactions)))
+print(NN)
+"""
+Add a test with no use of the NN Predictor.
+"""
