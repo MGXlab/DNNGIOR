89d4b52f8984e9a97d6bdc7f15d67db6898c4b98
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2023-09-27 18:12:07 +0200

    add gurobipy wls license credentials

--- .github/workflows/ci_test.py
+++ .github/workflows/ci_test.py
@@ -1,5 +1,16 @@
 import dnngior
+import gurobipy as gp
 import os
+
+# Gurobipy WLS credentials; for how to get one check: 
+# https://support.gurobi.com/hc/en-us/articles/13232844297489-How-do-I-set-up-a-Web-License-Service-WLS-client-license
+params = {}
+params["WLSACCESSID"] = "cb73b00e-795f-440e-b0b5-32d7f704cb13"
+params["WLSSECRET"]   = "a86dbc31-628d-4f7c-a5b5-a8de7016f29a"
+params["LICENSEID"]   = 964844
+
+gp.Env(params = params)
+
 
 base_path = "/".join(os.path.abspath(__file__).split("/")[:-3])
 
