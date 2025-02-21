c98534a7011bd04f2a0a2b03cb8ab53fa92a309c
Biomerene <hijisktr@gmail.com>
2025-02-21 18:46:24 +0100

    added comments explaining functions and updated error message

--- dnngior/fasta2model_CLI.py
+++ dnngior/fasta2model_CLI.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 __author__ = 'M.D. Boer'
 __version__ = '0.2'
 __date__ = '15 Sept, 2024'
@@ -116,7 +115,23 @@ def parse_arguments():
 
     return args
 
+
 def build_base_model(args):
+    """
+    Builds a base metabolic model from a given FASTA file.
+    0. Loads the required modelseedpy packages so they are only loaded when building base models
+    1. Creates an MSGenome object from the provided FASTA file.
+    2. Annotates the genome using the RAST (Rapid Annotations using Subsystems Technology) service.
+    3. Builds a base metabolic model using the MSBuilder class.
+    Args:
+        args (argparse.Namespace): The command-line arguments parsed by the argparse module. 
+            It should contain the following attributes:
+            - path_to_fasta (str): Path to the input FASTA file.
+            - model_name (str): Name of the model to be created.
+    Returns:
+        cobra.Model: The base metabolic model created from the input FASTA file.
+        SystemExit: If there is an error in creating the MSGenome object or annotating the genome.
+    """
     from modelseedpy import MSBuilder, MSGenome
     from modelseedpy.core import msmedia
     from modelseedpy.core.rast_client import RastClient
@@ -140,6 +155,21 @@ def build_base_model(args):
     return base_model
 
 def build_gapfilled_model_from_fasta(args):
+    """
+    Build a gap-filled metabolic model from a FASTA file.
+    This function constructs a base metabolic model from a given FASTA file and then performs gap-filling to ensure the model's functionality. If a base model already exists, it skips the base model construction step.
+        Args: An object containing the following attributes:
+            - path_to_base_model (str): Path to the base model file.
+            - output_folder (str): Directory where the output files will be saved.
+            - model_name (str): Name of the model.
+            - suffix_model (str): Suffix to be added to the model name.
+    Returns:
+        None
+    Raises:
+        FileNotFoundError: If the base model file does not exist and cannot be created.
+        Exception: If there is an error during the gap-filling process.
+    """
+
     from cobra.io import write_sbml_model
     args.path_to_base_model = os.path.join(args.output_folder,'base_models',f'base_{args.model_name}{args.suffix_model}')
     if not os.path.isfile(args.path_to_base_model):
@@ -150,6 +180,24 @@ def build_gapfilled_model_from_fasta(args):
     gapfill_model_wrapper(args)
 
 def gapfill_model_wrapper(args):
+    """
+    Gapfills a metabolic model and writes the gapfilled model to an SBML file:
+
+    Args: An object containing the following attributes:
+        - path_to_base_model (str): Path to the base model file.
+        - output_folder (str): Path to the output folder where the gapfilled model will be saved.
+        - model_name (str): Name of the model.
+        - suffix_model (str): Suffix to be added to the model name.
+        - medium (str, optional): Path to the medium file. If not provided, 'Complete' medium is assumed.
+        - gf_data_file (file object): File object to write gapfilling data.
+
+    Writes:
+    - An SBML file of the gapfilled model to the specified output folder.
+    - Gapfilling data to the specified gf_data_file.
+    Logs:
+    - A warning if the gapfilled model already exists.
+    """
+
     from cobra.io import write_sbml_model
     from dnngior.gapfill_class import Gapfill
     from dnngior.reaction_class import Reaction
@@ -173,6 +221,25 @@ def gapfill_model_wrapper(args):
 
 
 def create_output_folder(args):
+    """
+    Creates necessary output folders for base models and gapfilled models based on the provided arguments.
+    Args:
+        args: An object containing the following attributes:
+            - output_folder (str): The path to the main output folder.
+            - fasta_folder (bool): A flag indicating whether a fasta folder is provided.
+    Behavior:
+        - If the parent directory of the output folder exists:
+            - If `fasta_folder` is True, creates a 'base_models' folder inside the output folder if it doesn't already exist.
+            - Creates a 'gapfilled_models' folder inside the output folder if it doesn't already exist.
+        - If the parent directory of the output folder does not exist, the function exits with an error message.
+    Prints:
+        - Warnings if the 'base_models' or 'gapfilled_models' folders already exist.
+        - Messages indicating the creation of the 'gapfilled_models' folder.
+    Exits:
+        - If the parent directory of the output folder does not exist, exits the program with an error message.
+    """
+
+
     if os.path.exists(os.path.dirname(args.output_folder)):
         if args.fasta_folder:
             base_model_folder = os.path.join(args.output_folder, 'base_models')
@@ -197,6 +264,33 @@ def create_output_folder(args):
         sys.exit('# ERROR: your output_folder should have parents')
 
 def main():
+    """
+    Main function to parse arguments, create output folders, and build and gapfill models.
+    This function performs the following steps:
+    1. Parses command-line arguments.
+    2. Creates necessary output folders.
+    3. Sets up logging configuration.
+    4. Opens a file to write gapfill data.
+    5. Depending on the provided arguments, either:
+    a. Builds and gapfills models from a folder containing FASTA files.
+    b. Gapfills existing models from a folder containing base models.
+    6. Logs the progress and results of the model building and gapfilling process.
+    Command-line arguments:
+    - Required choice:
+        -f, --fasta_files: Folder containing the FASTA files to build models for.
+        -m, --models: Folder containing the base model files.
+    - Required:
+        -o, --output: Path to the output folder.
+    - Optional:
+        -sf, --suffix_faa: Suffix of the protein FASTA files (default: .faa).
+        -sm, --suffix_model: Suffix of the ungapfilled models (default: .xml).
+        -e, --medium: Path to the medium file.
+        -v, --version: Print version information and exit.
+        -h, --help: Show help message and exit.
+    Raises:
+        SystemExit: If no FASTA files or model files are found in the specified folders.
+        SystemExit: If the output folder does not have parent directories.
+    """
     args = parse_arguments()
     create_output_folder(args)
     logging.basicConfig(filename=os.path.join(args.output_folder, 'gapfill.log'), filemode='a', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
@@ -242,7 +336,7 @@ def main():
             gapfill_model_wrapper(args)
         print('# Done')
     else:
-        sys.exit('I dont think this message can show, if it does, you (or more likely me) did something weird')
+        sys.exit('Either a fasta_folder or a model_folder needs to be provided')
     args.gf_data_file.close()
 
 if __name__ == '__main__':
