e89a41fb570e1493ccf3db13bdee3c52d00b83c2
Biomerene <hijisktr@gmail.com>
2025-05-23 18:24:10 +0200

    seperated the tests so one can be used without tensorflow

--- tests/test.py
+++ tests/test.py
@@ -46,18 +46,6 @@ gapfill_compl_bg      = Gapfill(draftModelBiGG, objectiveName='Growth', dbType =
 Nit_media_file = os.path.join(base_path, 'docs/biochemistry/Nitrogen-Nitrite_media.tsv')
 gapfill_nitr     = Gapfill(draftModelMS, medium_file = Nit_media_file)
 
-#Example 3. training a network
-
-NN_path = os.path.join(path.parent,'docs', 'NN')
-data = pd.read_csv(os.path.join(NN_path, 'Sample_reaction_presence.csv'), index_col=0)
-network = NN_Trainer.train(data=data, modeltype='ModelSEED',output_path=os.path.join(NN_path,'custom_networks','test.npz'), save=True)
-tensor_network = NN_Trainer.train(data=data, modeltype='ModelSEED',return_full_network=True, save=False)
-#Custom network
-Gapfill(draftModelMS, trainedNNPath=os.path.join(NN_path, "custom_networks","test.npz"))
-model = cobra.io.read_sbml_model(draftModelMS)
-tensor_network.predict(model)
-network.predict(model)
-
 #
 # for reaction in gapfill_nitr.added_reactions:
 #     print(reaction)
